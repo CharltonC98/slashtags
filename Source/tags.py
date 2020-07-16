5c1ecaff2c8583be6ba608acf4ac22e11213db66
Cameron Charlton <charltonc98@gmail.com>
2020-07-16 13:49:27 +0100

    Update tags.py
    data structures ported to pandas.
    dataframe stores statistical video data for later expansion.

--- Source/tags.py
+++ Source/tags.py
@@ -1,58 +1,66 @@
 from apiclient.discovery import build 
+import pandas as pd
+import numpy as np
 import operator
 import re
    
-DEVELOPER_KEY = "AIzaSyAgIIprwuI3TbBBpPW1AB_sdo-CVq5FkVQ" 
+DEVELOPER_KEY = "" 
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
    
 youtube_object = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, 
                                         developerKey = DEVELOPER_KEY) 
    
-# returns list of video descriptions from youtube search request
+# returns a dataframe of video information from a search query
 def youtube_search_keyword(query, max_results):    
     search_request = youtube_object.search().list(
     q=query,
     part='id,snippet',
     maxResults=max_results).execute()
 
-    videos = []
-    descriptions = []
+    videosDf = pd.DataFrame(columns = ['videoId', 'description', 'descriptionTags', 'tags', 'viewCount', 'likeCount', 'dislikeCount'])
 
     for search_result in search_request.get('items', []):
         if search_result['id']['kind'] == 'youtube#video':
-            videos.append(search_result['id']['videoId'])
+            videosDf = videosDf.append({'videoId' : search_result['id']['videoId']}, ignore_index=True)
 
-    for video in videos:
+    for (_ , row) in videosDf.iterrows():
         video_request = youtube_object.videos().list(
-            part="snippet,contentDetails,statistics",
-            id= video)
-    
+        part="snippet,statistics",
+        id= row['videoId'])
+
         response = video_request.execute()
-        descriptions.append(response['items'][0]['snippet']['description'])
 
-    return descriptions
+        videosDf.loc[videosDf.videoId == row['videoId'], ['description', 'tags', 'viewCount', 'likeCount', 'dislikeCount']] = \
+                                                                              response['items'][0]['snippet']['description'], \
+                                                                                     response['items'][0]['snippet']['tags'], \
+                                                                             response['items'][0]['statistics']['viewCount'], \
+                                                                             response['items'][0]['statistics']['likeCount'], \
+                                                                          response['items'][0]['statistics']['dislikeCount']  
 
-# returns list of tags found in video descriptions
-def extract_desc_tags(descriptions):
-    hashtags = []
-    for desc in descriptions:
-        hashtags.append(re.findall(r"#(\w+)", desc))
+    return videosDf
 
-    return hashtags
+# populates descriptionTags from video descriptions
+def extract_desc_tags(df):
+    for (_ , row) in df.iterrows():
+        tempDesc = re.findall(r"#(\w+)", row['description'])
+        testStr = ', '.join(tempDesc)
+        df.loc[df.description == row['description'], 'descriptionTags'] = testStr
+
+    return df
 
 # returns a dictionary of tags and their frequency
-def desc_tags_freq(tags):
+def desc_tags_freq(df):
     tagDict = {}
-
-    for tag in tags:
-        if tag:
-            for x in range (0,len(tag)):
-                if tag[x].lower() not in tagDict:
-                    tagDict[tag[x]] = 1
+    for (_ , row) in df.iterrows():
+        for match in re.findall(r"[^,\s][^\,]*[^,\s]*", row['descriptionTags']):
+            if match:
+                if match.lower() not in tagDict:
+                    tagDict[match] = 1
                 else:
-                    tagDict[tag[x].lower()] += 1
+                    tagDict[match.lower()] += 1
 
     return sorted(tagDict.items(), reverse = True, key=operator.itemgetter(1))
 
-print(desc_tags_freq(extract_desc_tags(youtube_search_keyword('valorant', 100))))
+# test run
+print(desc_tags_freq(extract_desc_tags(youtube_search_keyword('valorant', 50))))
